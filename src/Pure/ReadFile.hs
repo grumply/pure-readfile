d3b98b412ab59291d4176a7a0189bfbaa2772d1c
Sean Hickman <sean@grumply.com>
2019-11-10 07:43:42 -0600

    Add filename to result of getFile.

--- src/Pure/ReadFile.hs
+++ src/Pure/ReadFile.hs
@@ -3,7 +3,7 @@ module Pure.ReadFile where
 
 import Pure.Data.Default
 import Pure.Data.Lifted
-import Pure.Data.Txt
+import Pure.Data.Txt as Txt
 
 import Control.Concurrent
 import Data.Maybe
@@ -13,23 +13,31 @@ foreign import javascript unsafe
   "var file = $1.files[$2]; var reader = new FileReader(); reader.readAsBinaryString(file); $r = reader;" get_file_reader_js :: Node -> Int -> IO JSV
 
 foreign import javascript unsafe
+  "$r = $1.files[$2].name" get_file_name_js :: Node -> Int -> IO Txt
+
+foreign import javascript unsafe
   "$r = $1.result" get_result_js :: JSV -> IO Txt
 #endif
 
-getFileAtIndex :: Node -> Int -> IO Txt
+getFileAtIndex :: Node -> Int -> IO (Txt,Txt)
 getFileAtIndex node n = do
 #ifdef __GHCJS__
   rdr <- get_file_reader_js node n
+  path <- get_file_name_js node n
   mv <- newEmptyMVar
   onRaw rdr "load" def $ \stop _ -> do
     result <- rdr ..# "result"
     putMVar mv result
     stop
   mresult <- takeMVar mv
-  return (fromMaybe def mresult)
+  let (name_reverse,_) = Txt.span sep (Txt.reverse path)
+      sep '/' = True
+      sep '\\' = True
+      sep _ = False
+  return (Txt.reverse name_reverse,fromMaybe def mresult)
 #else
-  return ""
+  return ("","")
 #endif
 
-getFile :: Node -> IO Txt
+getFile :: Node -> IO (Txt,Txt)
 getFile node = getFileAtIndex node 0
