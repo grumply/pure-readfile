c314c3c0777c0faa810d5c29765bd8b7dd3c3930
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:00 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-json, pure-lifted, pure-txt, text, hashable, stdenv }:
 mkDerivation {
   pname = "pure-readfile";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-json pure-lifted pure-txt text hashable ];
   homepage = "github.com/grumply/pure-readfile";
